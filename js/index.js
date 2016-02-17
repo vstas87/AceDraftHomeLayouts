9cca0011d3e526bd5115582206c086c23362dcda
vstas87 <uberimba@yandex.ru>
2016-02-17 16:46:55 +0300

    second commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+jQuery(document).ready(function() {
+    jQuery('.hero-slider').revolution(
+        {
+            fullWidth:"on",
+            forceFullWidth:"on",
+            startheight: 687,
+            startwidth: 1500,
+        });
+});
