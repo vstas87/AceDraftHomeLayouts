1f6566ab709dd2fb7344bbce2530dce6dc0fd5e8
vstas87 <stas@rockbeamsolutions.com>
2016-02-18 05:20:41 +0300

    new

--- js/index.js
+++ js/index.js
@@ -2,9 +2,27 @@
 jQuery(document).ready(function() {
     jQuery('.hero-slider').revolution(
         {
-            fullWidth:"on",
-            forceFullWidth:"on",
-            startheight: 687,
-            startwidth: 1500,
+            fullWidth:"off",
+            fullScreen:"on",
+            fullScreenOffsetContainer: ""
         });
+    handleHeader();
 });
+
+function handleHeader() {
+    // jQuery to collapse the navbar on scroll
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 110) {
+        $('.navbar').addClass('fixed-nav');
+    }
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 110) {
+            $('.navbar').addClass('fixed-nav');
+        } else {
+            $('.navbar').removeClass('fixed-nav');
+        }
+    });
+}
